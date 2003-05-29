fdae2ac11e36fdca0cab3cbac5a7788268b210bd
jadestorm <jadestorm>
2003-05-29 14:55:16 +0000

    Removed apache2 separate source tree.

--- apache2/hooks.c
+++
@@ -1,5 +0,0 @@
-void kerb_register_hooks(apr_pool_t *p)
-{
-	ap_hook_check_user_id(kerb_authenticate_user, NULL, NULL, APR_HOOK_MIDDLE);
-	ap_hook_access_checker(kerb_check_user_access, NULL, NULL, APR_HOOK_MIDDLE);
-}
